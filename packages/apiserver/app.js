6e669a777f234d60ea60a0754fbb049088be1255
kkojaeh <kkojaeh@gmail.com>
2021-05-03 10:12:26 +0900

    오류 처리를 전역으로 처리(sentry 와 같은 에러관리를 도입할 수 있도록 함)

--- packages/apiserver/app.js
+++ packages/apiserver/app.js
@@ -8,6 +8,7 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 
 i18n.configure({
   locales: ['en', 'ja', 'ko'],
+  // eslint-disable-next-line prefer-template,no-path-concat
   directory: __dirname + '/locales',
 });
 
@@ -27,7 +28,10 @@ function initApp(app) {
     CARRIERS[name] = require(`./carriers/${name}`);
   });
 
-  const CARRIERS_INFOS = Object.keys(CARRIERS).map(id => ({ id, ...CARRIERS[id].info }));
+  const CARRIERS_INFOS = Object.keys(CARRIERS).map(id => ({
+    id,
+    ...CARRIERS[id].info,
+  }));
 
   app.get('/carriers', (req, res) => {
     res.json(CARRIERS_INFOS);
@@ -35,10 +39,9 @@ function initApp(app) {
 
   app.get('/carriers/:carrierId', (req, res) => {
     if (!(req.params.carrierId in CARRIERS)) {
-      res.status(404).json({
-        message: res.__('not supported carrier'),
-      });
-      return;
+      const error = new Error('not supported carrier');
+      error.code = 404;
+      throw error;
     }
 
     res.json({
@@ -47,14 +50,13 @@ function initApp(app) {
     });
   });
 
-  app.get('/carriers/:carrierId/tracks/:trackId', (req, res) => {
+  app.get('/carriers/:carrierId/tracks/:trackId', (req, res, next) => {
     const { carrierId, trackId } = req.params;
 
     if (!(carrierId in CARRIERS)) {
-      res.status(404).json({
-        message: res.__('not supported carrier'),
-      });
-      return;
+      const error = new Error('not supported carrier');
+      error.code = 404;
+      throw error;
     }
 
     CARRIERS[carrierId]
@@ -68,13 +70,15 @@ function initApp(app) {
           },
         })
       )
-      .catch(err =>
-        res.status(typeof err.code === 'number' ? err.code : 500).json({
-          message: err.message
-            ? err.message
-            : res.__('error message'),
-        })
-      );
+      .catch(err => next(err));
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  app.use(function(err, req, res, next) {
+    res.status(err.code || 500).json({
+      // eslint-disable-next-line no-underscore-dangle
+      message: err.message ? err.message : res.__('error message'),
+    });
   });
 
   return app;
